6da096895f7d206d106d3946714dc23f85f1b2c0
Joy097 <shihabrumeechowdhury@gmail.com>
2023-12-15 18:47:04 +0600

    update 4

--- project/tasks/urls.py
+++ project/tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Tasklist
+from .views import Tasklist, TaskDetails
 
 urlpatterns = [
     path('', Tasklist.as_view(),name='tasks'),
+    path('task/<int:pk>/', TaskDetails.as_view(),name='tasks')
     
     ]
--- project/tasks/views.py
+++ project/tasks/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+from django.views.generic.list import DetailView
 from .models import Task
 
 class Tasklist(ListView):
+    model = Task
+    context_object_name = 'tasks'
+    
+class TaskDetails(DetailView):
     model = Task
      
