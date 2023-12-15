774d66a0ca61ab84650587121c6926906a7be1e3
Joy097 <shihabrumeechowdhury@gmail.com>
2023-12-15 19:02:07 +0600

    update 5

--- project/tasks/urls.py
+++ project/tasks/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import Tasklist, TaskDetails
+from .views import Tasklist, TaskDetails, TaskCreate
 
 urlpatterns = [
     path('', Tasklist.as_view(),name='tasks'),
-    path('task/<int:pk>/', TaskDetails.as_view(),name='tasks')
-    
+    path('task/<int:pk>/', TaskDetails.as_view(),name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
     ]
--- project/tasks/views.py
+++ project/tasks/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
-from django.views.generic.list import DetailView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from .models import Task
+from django.urls import reverse_lazy
+
 
 class Tasklist(ListView):
     model = Task
@@ -10,4 +13,9 @@ class Tasklist(ListView):
     
 class TaskDetails(DetailView):
     model = Task
-     
+    context_object_name = 'task'
+    
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
